f79ef146b1242f3772138b25fa6a75e938acd824
Khelifi Noureddine <khelifinoureddine10@gmail.com>
2021-04-15 17:35:22 +0000

    after creation a branch first convert settings.gradle file to settings.gradle.kts others details written inside the file in the end after changes we synchronize it

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+
+// Before changes
+
+//include ':app'
+//rootProject.name = "the_chance_week6"
+
+// After changes
+
+/*replace single quotes with double
+ and use kotlin syntax as parentheses
+*/
+include ( ":app" )
+rootProject.name = "the_chance_week6"
